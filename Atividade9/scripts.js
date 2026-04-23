2ef7b63ee98896cbca67a15d461140cacd2aeddc
Wixte <101189748+Wixte@users.noreply.github.com>
2026-04-22 23:35:22 -0300

    Add files via upload

--- Atividade9/scripts.js
+++ Atividade9/scripts.js
@@ -45,3 +45,53 @@ function diaDaSemana(dataString) {
 
   return diasDaSemana[diaNumerico];
 }
+
+let num1 = Number(prompt('Função 1 - Maior de três\nDigite o 1º número:'));
+let num2 = Number(prompt('Função 1 - Maior de três\nDigite o 2º número:'));
+let num3 = Number(prompt('Função 1 - Maior de três\nDigite o 3º número:'));
+
+let resultadoMaior = maiorNum(num1, num2, num3);
+alert(`O maior número entre ${num1}, ${num2} e ${num3} é: ${resultadoMaior}`);
+
+// -----
+
+let n1 = Number(prompt('Função 2 - Ordem Crescente\nDigite o 1º número:'));
+let n2 = Number(prompt('Função 2 - Ordem Crescente\nDigite o 2º número:'));
+let n3 = Number(prompt('Função 2 - Ordem Crescente\nDigite o 3º número:'));
+
+let resultadoOrdem = ordenCrescente(n1, n2, n3);
+alert(`Os números em ordem crescente são: ${resultadoOrdem}`);
+
+// -----
+
+let textoPalindromo = prompt(
+  'Função 3 - Palíndromo\nDigite uma palavra ou frase:',
+);
+
+let ePalindromo = palindromo(textoPalindromo);
+if (ePalindromo) {
+  alert(`A expressão "${textoPalindromo}" É um palíndromo!`);
+} else {
+  alert(`A expressão "${textoPalindromo}" NÃO é um palíndromo.`);
+}
+
+// -----
+
+let palavraPrincipal = prompt(
+  'Função 4 - Subconjunto\nDigite a palavra principal:',
+);
+let subPalavra = prompt(
+  'Função 4 - Subconjunto\nDigite a palavra que deseja verificar se está contida na primeira:',
+);
+
+let resultadoSubconjunto = verificaSubconjunto(palavraPrincipal, subPalavra);
+alert(`Verificação de subconjunto: O resultado "${resultadoSubconjunto}"`);
+
+// -----
+
+let dataTexto = prompt(
+  'Função 5 - Dia da Semana\nDigite uma data no formato Ano-Mês-Dia (exemplo: 2001-09-11):',
+);
+
+let resultadoDia = diaDaSemana(dataTexto + 'T12:00:00Z');
+alert(`A data ${dataTexto} cai em um(a): ${resultadoDia}`);
